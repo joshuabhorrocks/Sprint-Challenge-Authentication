c5371f3ef2ea28e2beb347756f02f07f8a33a487
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-05-22 11:33:38 -0600

    Authenticated with JWT and tested 3 endpoints twice each

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,79 @@
-const router = require('express').Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-  // implement registration
+const router = require("express").Router();
+
+const Jokes = require("../jokes/jokes-model.js");
+const { isValid } = require("../jokes/jokes-service.js");
+
+router.post("/register", (req, res) => {
+  const credentials = req.body;
+
+  if (isValid(credentials)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    // hash the password
+    const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+    credentials.password = hash;
+
+    // save the user to the database
+    Jokes.add(credentials)
+      .then(user => {
+        const token = createToken(user);
+
+        res.status(201).json({data: user, token});
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide username and password and the password shoud be alphanumeric",
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (isValid(req.body)) {
+    Jokes.findBy({username: username})
+      .then(([user]) => {
+        // compare the password the hash stored in the database
+        if (user && bcryptjs.compareSync(password, user.password)) {
+          // produce (sign) and send the token
+          const token = createToken(user);
+
+          res.status(200).json({message: "Welcome to our API", token});
+        } else {
+          res.status(401).json({message: "Invalid credentials"});
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message: "Please provide username and password and the password shoud be alphanumeric",
+    });
+  }
 });
+
+function createToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username,
+    role: user.role
+};
+
+const secret = process.env.JWT_SECRET || "thesecretsauce";
+
+  const options = {
+    expiresIn: "1d"
+}
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
